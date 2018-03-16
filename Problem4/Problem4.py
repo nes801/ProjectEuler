535435892bf1ad4e73e0694c4190d26722a1c3fc
nes801 <halbin801@gmail.com>
2018-03-16 16:41:15 +0900

    Problem4の新規追加

---
+++ Problem4/Problem4.py
@@ -0,0 +1,17 @@
+# 左右どちらから読んでも同じ値になる数を回文数という.
+# 2桁の数の積で表される回文数のうち, 最大のものは 9009 = 91 × 99 である.
+#
+# では, 3桁の数の積で表される回文数の最大値を求めよ.
+
+max=1
+for i in range(999,99,-1):
+    for j in range(999,99,-1):
+        #積を文字列に変換
+        num=str(i*j)
+        #文字列を反転
+        reverse=num[::-1]
+        if(num==reverse):
+            #最大値だったら更新
+            max= i*j if i*j > max else max
+            break
+print(max)
